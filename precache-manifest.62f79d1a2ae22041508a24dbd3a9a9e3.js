740967b4a74f710b6cb029aa7d3d633d94e0d16f
Fahad Noor <fahad.noor91@gmail.com>
2022-06-27 05:00:25 +0000

    Updates

---
+++ precache-manifest.62f79d1a2ae22041508a24dbd3a9a9e3.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3cd2bb5ce305207b80258b7441863f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "150a5625b252b761a6ca",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "03bba90ddfcb5aba7167",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "7943540f5a84ef05d420",
+    "url": "/static/css/main.baac0d80.chunk.css"
+  },
+  {
+    "revision": "689dc8ccbdd957af3e69",
+    "url": "/static/js/2.8d183aaf.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/static/js/2.8d183aaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "150a5625b252b761a6ca",
+    "url": "/static/js/3.4e44b961.chunk.js"
+  },
+  {
+    "revision": "03bba90ddfcb5aba7167",
+    "url": "/static/js/4.6d9e7a09.chunk.js"
+  },
+  {
+    "revision": "7943540f5a84ef05d420",
+    "url": "/static/js/main.57305da6.chunk.js"
+  },
+  {
+    "revision": "7b2fc0342af162b4338b",
+    "url": "/static/js/runtime-main.71ade5ed.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "54db718aaaacf8337d360bbab92a976a",
+    "url": "/static/media/arbisoft-logo.54db718a.jpeg"
+  },
+  {
+    "revision": "7368707e0847955846421bef28c8a39d",
+    "url": "/static/media/bsv-product.7368707e.jpeg"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e47d75c4c9365cef37bd4c1ab5901948",
+    "url": "/static/media/geniteam-logo.e47d75c4.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "6774f8e08c32a21d45b6c2255d7aca07",
+    "url": "/static/media/insurify-product.6774f8e0.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "6bdcf54ef3ad4eff0ce1d80425721e1b",
+    "url": "/static/media/nu-logo.6bdcf54e.jpeg"
+  },
+  {
+    "revision": "2454f2f215d1b4ea0e38801d6f56c5e8",
+    "url": "/static/media/ohi-product.2454f2f2.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
